9701182f06921243b95c8bdee0c161e2fbfe93c1
emirtopaloglu0 <emirt2048@gmail.com>
2024-05-24 14:44:25 +0300

    View Componentler eklendi

---
+++ MyPortfolio/ViewComponents/_AboutComponentPartial.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Security.Policy;
+
+namespace MyPortfolio.ViewComponents
+{
+    public class _AboutComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() {  return View(); }
+    }
+}
