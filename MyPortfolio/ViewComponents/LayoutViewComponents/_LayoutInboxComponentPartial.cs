30508b2545653b6e541a8ebdcad787b57629e846
emirtopaloglu0 <emirt2048@gmail.com>
2024-05-26 16:17:20 +0300

    Mesaj bildirim kutusu düzenlendi.

---
+++ MyPortfolio/ViewComponents/LayoutViewComponents/_LayoutInboxComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
+
+namespace MyPortfolio.ViewComponents.LayoutViewComponents
+{
+    public class _LayoutInboxComponentPartial : ViewComponent
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+
+
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.messageCount = context.Messages.Where(x => x.IsRead == false).Count();
+            var values = context.Messages.Where(x=> x.IsRead == false).ToList();
+            return View(values);
+        }
+    }
+}
