1411c4b15b0a93ea7ba6460660fe7a4d821ab435
emirtopaloglu0 <emirt2048@gmail.com>
2024-05-28 11:33:08 +0300

    Dashboard'a deneyimler ve yetenekler eklendi. Yetenek ekleme, silme ve düzenleme için ileriye dönük notlar eklendi.

--- MyPortfolio/Controllers/LayoutController.cs
+++ MyPortfolio/Controllers/LayoutController.cs
@@ -1,11 +1,14 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MyPortfolio.DAL.Context;
 
 namespace MyPortfolio.Controllers
 {
     public class LayoutController : Controller
     {
+        MyPortfolioContext context = new MyPortfolioContext();
         public IActionResult Index()
         {
+            ViewBag.v1 = context.Messages.Where(x => x.IsRead == false).Count();
             return View();
         }
     }
--- MyPortfolio/Controllers/StatisticController.cs
+++ MyPortfolio/Controllers/StatisticController.cs
@@ -13,6 +13,8 @@ namespace MyPortfolio.Controllers
             ViewBag.v3 = context.ToDoLists.Count();
             ViewBag.v4 = context.Messages.Where(x => x.IsRead == true).Count();
             ViewBag.v5 = context.Messages.Where(x => x.IsRead == false).ToList();
+            ViewBag.v6 = context.Experiences.ToList();
+            ViewBag.v7 = context.Skills.ToList();
 
             var values = context.ToDoLists.ToList();
 
