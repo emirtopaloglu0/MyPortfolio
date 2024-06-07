1314dc1006f3eb293b86b03cc37910c6286f2346
emirtopaloglu0 <emirt2048@gmail.com>
2024-06-07 10:32:08 +0300

    Sosyal Medya sayfası eklendi.

---
+++ MyPortfolio/Controllers/SocialMediaController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using MyPortfolio.DAL.Context;
+using MyPortfolio.DAL.Entities;
+
+namespace MyPortfolio.Controllers
+{
+    public class SocialMediaController : Controller
+    {
+        MyPortfolioContext context = new MyPortfolioContext();
+        public IActionResult Index()
+        {
+            var values = context.SocialMedias.ToList();
+            return View(values);
+        }
+
+
+        [HttpGet]
+        public IActionResult CreateMedia()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult CreateMedia(SocialMedia socialMedia)
+        {
+            context.SocialMedias.Add(socialMedia);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DeleteMedia(int id)
+        {
+            var value = context.SocialMedias.Find(id);
+            context.SocialMedias.Remove(value);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public IActionResult UpdateMedia(int id) 
+        {
+            var value = context.SocialMedias.Find(id);
+            return View(value);
+        }
+
+
+        [HttpPost]
+        public IActionResult UpdateMedia(SocialMedia socialMedia)
+        {
+            context.SocialMedias.Update(socialMedia);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
